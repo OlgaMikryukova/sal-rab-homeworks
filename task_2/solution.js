3566174a1cc53d283ab1004666b4305ae3648a0c
OlgaMikryukova <omikryukova@iitdgroup.ru>
2021-10-26 20:32:30 +0300

    task 2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -19,12 +19,16 @@ function calcShipping(sum, min, shipping) {
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
 
-    if (productsSum >= freeShippingMinSum) shippingSum == (0);
+    if (productsSum >= freeShippingMinSum) {
+        shippingSum = (0);
+    }
 
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
 
-    if (productsSum > 0 + < freeShippingMinSum) shippingSum == (shippingPrice);
+    if (productsSum > 0 + < freeShippingMinSum) {
+        shippingSum = shippingPrice;
+    }
 
     // Конец решения задания №2.1.
 
