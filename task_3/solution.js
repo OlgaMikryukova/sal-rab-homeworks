d677c4b39554e6ebf4c310013b39a2caf367b92f
OlgaMikryukova <omikryukova@iitdgroup.ru>
2021-10-27 23:50:42 +0300

    task 3 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -57,5 +57,5 @@ function renderCartItem(item) {
 
     // Конец решения задания №3.1.
 
-    return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
+    return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`.;
 }
