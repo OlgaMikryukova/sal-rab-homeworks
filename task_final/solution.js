1faa299d476abb03258e999b82003140ac71b3d3
OlgaMikryukova <omikryukova@iitdgroup.ru>
2021-11-16 17:47:38 +0300

    task f done

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,21 +33,26 @@
 //    }
 // }
 
+
+
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: {name, phone}, goods: ({title, count}), order: {address, sum}};
+   
+    let client = name + ' ' + phone;
+
+    let order = {address: address, sum: sum};
+
+    let goods = [];
+  
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i]);
+    for (let i = 0; i < countOfGoods; i += 1) {
+     //   data.goods.push(goods[i]);
+        let good = {title: goods[i].title, count: goods[i].count};
+        goods.push(good);
     }
 
-
-    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + "подъезд, " + address.entrance + " этаж, кв " + address.flat;
-
-    data.order.sum = sum;
-
-    data.client = name + " " + phone;
+    let data = {client: client, order: order, goods: goods};
 
     let jsonData = JSON.stringify(data);
 
