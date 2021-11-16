d256c27ba887a15d0642f8d488aa26a0dc3add76
OlgaMikryukova <omikryukova@iitdgroup.ru>
2021-11-16 11:45:51 +0300

    task 4 done

--- task_4/solution.js
+++ task_4/solution.js
@@ -15,7 +15,9 @@ function parseProducts(json) {
 function renderProductsCards(json) {
 
     clearProducts();
-    parseProducts(json); // Помоему тут ошибка в задании, должен быть не clearProducts, а просто products
+
+    clearProducts = parseProducts(json); 
+
     let length = products.length
     for (let i = 0; i <= length - 1; i++) {
         addProduct(products[i])
